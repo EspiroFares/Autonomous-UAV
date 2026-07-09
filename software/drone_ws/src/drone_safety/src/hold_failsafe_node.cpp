1b45596d174f31d95927caaa336f54365aa88d7b
Fares Espiro <faresespiro535@gmail.com>
2026-07-09 13:36:16 +0200

    started working on drone_safety nodes

---
+++ software/drone_ws/src/drone_safety/src/hold_failsafe_node.cpp
@@ -0,0 +1,9 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <cmath>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "drone_interfaces/msg/control_setpoint.hpp"
